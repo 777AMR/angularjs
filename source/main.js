298386a5c5d97202a550b09eadcaf9ece9982bea
777AMR <meyram.ramazanov@gmail.com>
2022-10-18 14:48:47 +0600

    9. Ограничения (Restrictions) в директивах

--- source/main.js
+++ source/main.js
@@ -1,23 +1,10 @@
 var app = angular.module('app', []);
 
-app.controller('mainCtrl', function ($scope) {
-    $scope.money1 = '1.22$';
-    $scope.money2 = '$2.33';
-    $scope.money3 = '4.33';
-});
-
-app.filter('moneyFilter', function () {
-    return function (str) {
-        var lastChar = str.slice(-1);
-        var firstChar = str.slice(0, 1);
-
-        if (lastChar === '$') {
-            slicedPart = str.slice(0, str.length - 1);
-            return '$' + slicedPart;
-        } else if (firstChar === '$') {
-            return str;
-        } else {
-            return '$' + str;
+app.directive('fooBar', function(){
+    return {
+        restrict: 'EACM',
+        link: function(){
+            console.log('fooBar');
         }
     }
-});
+})
