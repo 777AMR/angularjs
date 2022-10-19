29875f02567a09d6b7d78965d179decf58bf55ed
777AMR <meyram.ramazanov@gmail.com>
2022-10-19 12:19:24 +0600

    21. Ng-cloak и ng-bind

--- source/main.js
+++ source/main.js
@@ -1,18 +1,1 @@
 var app = angular.module('app', []);
-
-app.directive('uiSource', function () {
-    return {
-        compile: function (element) {
-            var escape = function (content) {
-                return content.replace(/\</g, '&lt;')
-                              .replace(/\>/g, '&gt;');
-            };
-
-            var pre = angular.element('<pre class="prettyprint"></pre');
-            var pretty = prettyPrintOne(escape(element.html()));
-            console.log(pretty);
-            pre.append(pretty);
-            element.replaceWith(pre);
-        }
-    };
-});
