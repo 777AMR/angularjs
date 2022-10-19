c4cc19bae52cc6d865fb7a4c698a03c5b71c869f
777AMR <meyram.ramazanov@gmail.com>
2022-10-19 11:32:08 +0600

    19. Angular-mocks

--- source/main.js
+++ source/main.js
@@ -1,4 +1,30 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngMockE2E']);
+
+app.run(function ($httpBackend) {
+    var books = [
+        {
+            name: 'AngularJS'
+        },
+        {
+            name: 'ReactJS'
+        },
+        {
+            name: 'VueJS'
+        }
+    ];
+
+    $httpBackend.whenGET('http://localhost:3001/books').respond(200, books);
+
+    $httpBackend.whenPOST('http://localhost:3001/books').respond(function (method, url, data) {
+        console.log('method', method);
+        console.log('url', url);
+        console.log('data', data);
+
+        var result = JSON.parse(data);
+        books.push(result);
+        return[200, result];
+    });
+});
 
 app.controller('mainCtrl', function ($http, $scope) {
     $http.get('http://localhost:3001/books')
